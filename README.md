27f83d9043fc6cd4afc209ae3857d9a2d779cbda
airportyh <airportyh@gmail.com>
2013-10-19 07:10:12 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+ng-calc
+=======
+
+ng-calc is a set of AngularJS directives that makes it easy to make interactive spreadsheet applications using just HTML. 
---
+++ ng-calc.js
@@ -0,0 +1,70 @@
+angular.module('ng-calc', [])
+
+.directive('sum', function(){
+    return {
+      restrict: 'E',
+      controller: function($scope, $element){
+        var ngModel = $element.attr('ng-model')
+        var $for = $element.attr('for')
+        var match = $for.match(/([a-zA-Z][a-zA-Z0-9]*)\s+in\s+([a-zA-Z][a-zA-Z0-9]*)/)
+        if (!match){
+            console.error($for, 'didn\'t match')
+            return
+        }
+        var lhs = match[1]
+        var rhs = match[2]
+        var expr = $element.attr('expr')
+        set()
+        
+        $scope.$watch(set)
+        
+        function set(){
+            $scope[ngModel] = calcSum()
+            $element.text($scope[ngModel])
+        }
+        
+        function calcSum(){
+            var arr = $scope.$eval(rhs)
+            var sum = 0
+            for (var i = 0; i < arr.length; i++){
+                var scope = $scope.$new()
+                scope[lhs] = arr[i]
+                sum += Number(scope.$eval(expr) || 0)
+            }
+            return sum
+        }
+      }
+    }
+  })
+
+.directive('barchart', function(){
+    return {
+        restrict: 'E',
+        controller: function($scope, $element){
+            var ctx = $element.find('canvas')[0].getContext('2d')
+            var dataProp = $element.attr('data')
+            var labelProp = $element.attr('label')
+            var valueProp = $element.attr('value')
+            set()
+            $scope.$watch(set)
+            
+            function set(){
+                var arr = $scope.$eval(dataProp)
+
+                var values = []
+                var labels = []
+                for (var i = 0; i < arr.length; i++){
+                    labels.push(arr[i][labelProp])                        
+                    values.push(Number(arr[i][valueProp] || 0))
+                }
+                new Chart(ctx).Bar({
+                    labels: labels,
+                    datasets: [{
+                        data: values
+                    }]
+                })
+            }
+        },
+        template: '<canvas width="300" height="300"></canvas>'
+    }
+})
